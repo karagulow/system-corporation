349fff6841dc5881f0bf0b8086475557a3f16a50
Albert Karagulov <albert.karagulov@mail.ru>
2023-12-07 17:17:14 +0300

    the auth page has been created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 
 import './assets/scss/main.scss';
 import { Request } from './pages/Request';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="request" element={<Request />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 }
